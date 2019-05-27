f6519277387a160001686e82347f90614170b015
Nathan McCallum <me@1vasari.xyz>
2019-05-27 10:10:20 +1000

    Overhaul formatting

--- index.js
+++ index.js
@@ -2,29 +2,46 @@
 // Copyright Platformers (C) 2019
 //
 
-var Billow = {};
-
-Billow.date = {
-  SECOND: 1000,
-  MINUTE: 1000 * 60,
-  HOUR: 1000 * 60 * 60,
-  DAY: 1000 * 60 * 60 * 24,
-  WEEK: 1000 * 60 * 60 * 24 * 7,
-
-  getTimeInDay: function(time) {
-    var d = new Date(time);
-    return d.getTime() % Billow.date.DAY;
-  },
-
-  now: function() {
-    return (new Date()).getTime();
-  },
-
-  today: function() {
-    var now = new Date();
-    var d = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
-    return d.getTime();
-  }
+var B = {
+  date: {},
 };
 
-module.exports = Billow;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// DATE
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+B.date.SECOND = 1000;
+B.date.MINUTE = 1000 * 60;
+B.date.HOUR = 1000 * 60 * 60;
+B.date.DAY = 1000 * 60 * 60 * 24;
+B.date.WEEK = 1000 * 60 * 60 * 24 * 7;
+
+B.date.getTimeInDay = function(time) {
+  var d = new Date(time);
+  return d.getTime() % B.date.DAY;
+};
+
+B.date.now = function() {
+  return (new Date()).getTime();
+};
+
+B.date.today = function() {
+  var now = new Date();
+  var d = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
+  return d.getTime();
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Magically export B to the right places
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+if (global || (module && module.exports)) {
+  console.info('Attaching B.js to the module.exports object');
+  module.exports = B;
+} else {
+  console.info('Attaching B.js to the window object');
+  window.B = B;
+}
