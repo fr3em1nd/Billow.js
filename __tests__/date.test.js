4450ef807b0adc41386af430ec1e54e78be2395a
Nathan McCallum <me@1vasari.xyz>
2019-05-30 20:51:03 +1000

    Fix format

--- __tests__/date.test.js
+++ __tests__/date.test.js
@@ -2,25 +2,25 @@
 // Copyright Platformers (C) 2019
 //
 
-const Billow = require('./../index');
+const B = require('./../index');
 
-test('Billow.date.getTimeInDay', () => {
-  expect(Billow.date.getTimeInDay(1555595113082)).toEqual(49513082);
-  expect(Billow.date.getTimeInDay(0)).toEqual(0);
-  expect(Billow.date.getTimeInDay(Billow.date.HOUR)).toEqual(Billow.date.HOUR);
-  expect(Billow.date.getTimeInDay(Billow.date.HOUR * 25)).toEqual(Billow.date.HOUR);
+test('B.date.getTimeInDay', () => {
+  expect(B.date.getTimeInDay(1555595113082)).toEqual(49513082);
+  expect(B.date.getTimeInDay(0)).toEqual(0);
+  expect(B.date.getTimeInDay(B.date.HOUR)).toEqual(B.date.HOUR);
+  expect(B.date.getTimeInDay(B.date.HOUR * 25)).toEqual(B.date.HOUR);
 
-  const now = Billow.date.now();
-  const today = Billow.date.today();
+  const now = B.date.now();
+  const today = B.date.today();
   const tid = now - today + ((new Date()).getTimezoneOffset() * 60 * 1000);
-  expect(Billow.date.getTimeInDay(now)).toEqual(tid);
+  expect(B.date.getTimeInDay(now)).toEqual(tid);
 });
 
 test('B.date.commonDateString', () => {
-  expect(Billow.date.commonDateString(0)).toBe('01 Jan 1970');
-  expect(Billow.date.commonDateString(Billow.date.DAY * 10)).toBe('11 Jan 1970');
-  expect(Billow.date.commonDateString(Billow.date.DAY * 100)).toBe('11 Apr 1970');
-  expect(Billow.date.commonDateString(Billow.date.DAY * 1000)).toBe('27 Sep 1972');
-  expect(Billow.date.commonDateString(Billow.date.DAY * 10000)).toBe('19 May 1997');
-  expect(Billow.date.commonDateString(1559213379945)).toBe('30 May 2019');
+  expect(B.date.commonDateString(0)).toBe('01 Jan 1970');
+  expect(B.date.commonDateString(B.date.DAY * 10)).toBe('11 Jan 1970');
+  expect(B.date.commonDateString(B.date.DAY * 100)).toBe('11 Apr 1970');
+  expect(B.date.commonDateString(B.date.DAY * 1000)).toBe('27 Sep 1972');
+  expect(B.date.commonDateString(B.date.DAY * 10000)).toBe('19 May 1997');
+  expect(B.date.commonDateString(1559213379945)).toBe('30 May 2019');
 });
