953922646a436a53a6a250f70dbdf7dcb9ede442
Nathan McCallum <nmccallum7@gmail.com>
2019-06-26 23:54:36 +1000

    Create Billow.js deploy script

---
+++ script/deploy.js
@@ -0,0 +1,52 @@
+//
+// Copyright Platformers (C) 2019
+//
+
+const UpviseClient = require('upvise-client');
+const fs = require('fs');
+const path = require('path');
+
+const config = require('../config.json');
+const B = require('../index');
+const js = fs.readFileSync(path.join(__dirname, '..', 'index.js'));
+
+const deploy = async () => {
+  for (const database of config.databases) {
+    const {
+      email,
+      password,
+      url,
+      version,
+    } = database;
+
+    const upvise = new UpviseClient(url);
+    console.log('Looking at ' + email);
+    const user = await upvise.authenticate(email, password);
+    console.log('Signed into ' + user.companyName);
+
+    if (version === '*') {
+      console.log('Deploying latest version of Billow.js to ' + user.companyName);
+      await upvise.update('System.globalsettings', 'billow-software.billow.js', {
+        name: B.VERSION,
+        value: js,
+      });
+      continue;
+    }
+
+    const deployed = await upvise.selectId('System.globalsettings', 'billow-software.billow.js');
+    const deployedVersion = deployed.name;
+
+    if (B.VERSION === deployedVersion || B.VERSION === version) {
+      console.log('Deploying version ' + version + ' to ' + user.companyName);
+      await upvise.update('System.globalsettings', 'billow-software.billow.js', {
+        value: js,
+      });
+    } else {
+      console.log('Moving on. Specified version ' + version + ' is already deployed to ' + user.companyName);
+    }
+  }
+};
+
+if (require.main === module) {
+  deploy();
+}
